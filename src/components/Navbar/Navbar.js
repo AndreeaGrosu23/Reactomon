f14e54db6a9a62fabebaf0a4be98b8637afcd82a
Andreea Grosu <andreea.grosu87@gmail.com>
2020-07-26 22:48:33 +0200

    Fetchedapi using async and hooks

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            Pokemon List
+        </div>
+    );
+}
+
+export default Navbar;
