1225d3cce2666461e48f2173ee68944d033371ee
Andreea Grosu <andreea.grosu87@gmail.com>
2020-07-26 14:08:45 +0200

    create components

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  } 
+
+}
+
+export default App; 
---
+++ src/components/types.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class TypeList extends Component {
+    render() {
+        return (
+            <div>
+                pokemon types...
+            </div>
+        );
+    }
+}
