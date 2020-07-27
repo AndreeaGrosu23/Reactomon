ba68a56f56b710a5a67e7a6e37e49b5250819d9c
Andreea Grosu <andreea.grosu87@gmail.com>
2020-07-27 08:28:09 +0200

    Add nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import './style.css';
 
 function Navbar() {
     return (
         <div className="Navbar">
-            Pokemon List
+            <div className="submenu">
+                <NavLink exact to="/pokemons">
+                    PokeList
+                </NavLink>
+            </div>
+            <div className="submenu">
+                <NavLink exact to="/types">
+                    PokeTypes
+                </NavLink>
+            </div>
         </div>
     );
 }
